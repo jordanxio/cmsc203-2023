c7e41ee8514e8c7f1cb3dd398ad7fa14fdfd765c
jordanxio <113549445+jordanxio@users.noreply.github.com>
2023-09-25 14:07:14 -0400

    Add files via upload

---
+++ JXiong_Lab1/JXiong-Task1/MovieDriver.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MovieDriver {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+            Movie movie = new Movie();
+
+            System.out.println("Enter the title of the movie");
+            String title = input.nextLine();
+            movie.setTitle(title);
+
+            System.out.println("Enter the rating of the movie");
+            String rating = input.nextLine();
+            movie.setRating(rating);
+
+            System.out.println("Enter the number of tickets sold for this movie");
+            int ticketsSold = Integer.parseInt(input.nextLine());
+            movie.setSoldTickets(ticketsSold);
+
+            System.out.println(movie.toString());
+
+
+        System.out.println("Goodbye");
+        input.close();
+    }
+}
